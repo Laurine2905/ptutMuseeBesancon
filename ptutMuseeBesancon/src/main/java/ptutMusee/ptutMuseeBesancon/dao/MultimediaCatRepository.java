ac13413da5380fc72fc8faceae7569be5c42ed64
Laurine2905 <laurine.rat@outlook.fr>
2023-03-06 14:45:30 +0100

    dao

---
+++ ptutMuseeBesancon/src/main/java/ptutMusee/ptutMuseeBesancon/dao/MultimediaCatRepository.java
@@ -0,0 +1,8 @@
+package ptutMusee.ptutMuseeBesancon.dao;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MultimediaCatRepository extends JpaRepository<MultimediaCat, Integer> {
+
+}
---
+++ ptutMuseeBesancon/src/main/java/ptutMusee/ptutMuseeBesancon/dao/ObjetRepository.java
@@ -0,0 +1,14 @@
+package ptutMusee.ptutMuseeBesancon.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ObjetRepository extends JpaRepository<Objet, Integer> {
+
+    /**
+     * Trouve un objet par son nom.
+     * Cette méthode sera automatiquement implémentée par Spring Data JPA.
+     * @param ObjetName
+     * @return L'objet correspondant au nom fourni, ou null si pas trouvé.
+     */
+    public Objet findByName(String ObjetName);
+}
