b502d3f2105541ed59d9d7741238d064195c8d0b
Laurine2905 <laurine.rat@outlook.fr>
2023-03-06 14:28:42 +0100

    Initial commit

---
+++ ptutMuseeBesancon/src/test/java/ptutMusee/ptutMuseeBesancon/PtutMuseeBesanconApplicationTests.java
@@ -0,0 +1,13 @@
+package ptutMusee.ptutMuseeBesancon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtutMuseeBesanconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
